1b0874329585e1193871cc7c974cb15cccff8a54
Ardika Rommy Sanjaya <contact@ardikars.com>
2020-03-25 10:35:14 +0700

    Add session util

--- cmsnesia-accounts-model/src/main/java/com/cmsnesia/accounts.model/Session.java
+++ cmsnesia-accounts-model/src/main/java/com/cmsnesia/accounts.model/Session.java
@@ -1,7 +1,10 @@
 package com.cmsnesia.accounts.model;
 
 import java.io.Serializable;
+import java.util.Collections;
 import java.util.Set;
+import java.util.stream.Collectors;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -26,4 +29,29 @@ public class Session implements Serializable {
   private Set<EmailDto> emails;
 
   private Set<ApplicationDto> applications;
+
+  public static Set<String> applicationIds(Session session) {
+    Set<String> appIds =
+        session == null || session.getApplications() == null
+            ? Collections.emptySet()
+            : session.getApplications().stream()
+                .map(ApplicationDto::getId)
+                .collect(Collectors.toSet());
+    return appIds;
+  }
+
+  public static Set<ApplicationDto> applications(Session session) {
+    Set<ApplicationDto> applications =
+        session.getApplications() == null
+            ? Collections.emptySet()
+            : session.getApplications().stream()
+                .map(
+                    applicationDto ->
+                        ApplicationDto.builder()
+                            .id(applicationDto.getId())
+                            .name(applicationDto.getName())
+                            .build())
+                .collect(Collectors.toSet());
+    return applications;
+  }
 }
